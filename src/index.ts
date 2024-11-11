0e9a9cb2746d25c9b0c8656ba98875b626de9fb5
Radha Sharma <radhasharma2786@gmail.com>
2024-11-11 15:00:56 +0530

    basic setup done

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import { typeDefs } from './schema.js';
+
+const server = new ApolloServer({
+    typeDefs, // this tells what the graph should  look like
+    // resolvers,  // resolver functions are responsibel for handling incoming request and return data to the client 
+})
+
+const {url} = await startStandaloneServer(server, {
+    listen : { port: 4000},
+})
+
+console.log(`🚀 Server ready at: ${url}`);
